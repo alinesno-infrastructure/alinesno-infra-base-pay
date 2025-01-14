41264147486fa39b7a4b2b0abd06806b39b36037
张婷(Tina) <tinaAip@gmail.com>
2025-01-14 18:20:12 +0800

    feat:在AppConfiguration中添加CommandLineRunner实现及日志配置

--- alinesno-infra-base-pay-boot/src/main/java/com/alinesno/infra/base/pay/config/AppConfiguration.java
+++ alinesno-infra-base-pay-boot/src/main/java/com/alinesno/infra/base/pay/config/AppConfiguration.java
@@ -3,22 +3,33 @@ package com.alinesno.infra.base.pay.config;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.dtflys.forest.springboot.annotation.ForestScan;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 项目配置
  */
+@Slf4j
 @EnableActable
 @MapperScan("com.alinesno.infra.base.pay.mapper")
 @EnableInfraSsoApi
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer"
+})
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements CommandLineRunner {
 
     @Bean
     public LogAspect getLogAspect(){
         return new LogAspect() ;
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("AppConfiguration.run");
+    }
 }
